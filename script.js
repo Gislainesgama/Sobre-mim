3fb79963bfc99d92d64b2ec8e3e171277dff441f
italokk <gislainesgama@live.com>
2023-12-02 15:52:15 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+const modeSwitch = document.getElementById('modeSwitch');
+const body = document.body;
+
+modeSwitch.addEventListener('change', () => {
+    body.classList.toggle('dark-mode');
+});
+alert("Bem vindos ao meu projeto de estudo!")
+
